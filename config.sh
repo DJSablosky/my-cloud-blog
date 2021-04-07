b756c104140d4193922877d51bbd0d6f58bc7fb7
DJSablosky <DJSablosky@msn.com>
2021-04-07 12:14:52 -0400

    Removed files

--- config.sh
+++
@@ -1,3 +0,0 @@
-sed -i “s/ALGOLIA_API_KEY_PLACEHOLDER/${ALGOLIA_API_KEY}/g” config.toml
-sed -i “s/ALGOLIA_APP_ID_PLACEHOLDER/${ALGOLIA_APP_ID}/g” config.toml
-sed -i “s/ALGOLIA_INDEX_NAME_PLACEHOLDER/${ALGOLIA_INDEX_NAME}/g” config.toml
