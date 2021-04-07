d21ad62f591a598196c4bb1630b3ce7b9e73451c
DJSablosky <DJSablosky@msn.com>
2021-04-07 10:47:31 -0400

    Added config script

---
+++ config.sh
@@ -0,0 +1,3 @@
+sed -i “s/ALGOLIA_API_KEY_PLACEHOLDER/${ALGOLIA_API_KEY}/g” config.toml
+sed -i “s/ALGOLIA_APP_ID_PLACEHOLDER/${ALGOLIA_APP_ID}/g” config.toml
+sed -i “s/ALGOLIA_INDEX_NAME_PLACEHOLDER/${ALGOLIA_INDEX_NAME}/g” config.toml
